434b367ce77735e63253f8bbf93bcccb0d15592e
fevenbelay1240 <fb20meng@siena.edu>
2023-07-19 16:52:52 -0500

    implemented BST

--- algorithm/BST.java
+++ algorithm/BST.java
@@ -16,17 +16,47 @@ public class BST {
      * 
      * @param original array to be sorted
      */
-    public void sortedArr(int[] arr) {    
-        Arrays.sort(arr);
-        for (int n : arr) {
-            System.out.println(n);
+    // public void sortedArr(int[] arr) {
+    //     Arrays.sort(arr);
+    //     for (int n : arr) {
+    //         System.out.print(n);
+    //     }
+    // }
+
+    public int searchNum(int[] arr, int num) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        
+       // mid = (right + left) / 2;
+        while (left <= right) {
+         int mid  = (right + left) / 2;
+         int midNum = arr[mid];
+            if (midNum == num) {
+                return mid;
+            }
+          else  if (midNum < num) {
+               // left = 0;
+                right = mid - 1;
+              //  mid = (right + left) / 2;
+            } else if (midNum > num) {
+                left = mid+1;
+              //  right = arr.length - 1;
+            //    mid = (right + left) / 2;
+            }
         }
+        return -1;
+
     }
 
     public static void main(String[] args) {
         int[] nums = { 2, 8, 4, 6, 9, 3, 7 };
         BST b = new BST();
-        b.sortedArr(nums);
-       
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length;i++) {
+            System.out.print(nums[i]);
+        }  
+        System.out.println();
+        System.out.println(b.searchNum(nums, 3));
     }
 }
